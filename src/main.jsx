f25ed92e248b1fff25c8ea740735b9d4982f9ddf
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-08-06 23:28:28 +0530

    AppProvider tag added

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
+import { AppProvider } from './context/AppContext'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 )
