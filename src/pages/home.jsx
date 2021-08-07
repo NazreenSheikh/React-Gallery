76a6e7bcbcabe7944b662366720b662e72db154c
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-08-08 01:25:25 +0530

    render Image

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Images from '../components/Images'
 
 export default function Home() {
   return (
-    <div className="flex h-screen">
-      <h1 className="m-auto text-3xl">Welcome Home</h1>
-    </div>
+    <>
+      <h1 className="m-auto mb-3 text-3xl text-center mt-7">
+        Welcome To NG Gallery
+      </h1>
+      <section className="flex justify-center mx-auto text-center">
+        <div className="w-10/12">
+          <div className="text-center">
+            <Images />
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
