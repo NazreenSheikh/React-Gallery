8e8bd5851ae9b341a438386b20421af3304a82ea
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-08-08 20:37:39 +0530

    styled login file

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -26,31 +26,38 @@ const Login = () => {
   }
 
   return (
-    <div>
-      <div>
-        <form onSubmit={handleLogin}>
-          <h1>Please login</h1>
+    <div className="flex my-32  justify-center">
+      <div className=" w-5/12  text-white flex flex-wrap justify-center shadow-lg rounded-lg bg-gradient-to-br from-red-600 to-pink-700 ">
+        <form className="  w-10/12 " onSubmit={handleLogin}>
+          <h1 className="w-full text-4xl  tracking-widest text-center pt-10 pb-3">
+            Login Please{' '}
+          </h1>
           {error && <p>{error.message}</p>}
-          <div>
+          <div className="w-full my-6  text-black ">
             <input
               type="email"
-              className=""
               placeholder="enter your email"
               ref={emailRef}
               required
+              className="px-1 text-lg"
             />
           </div>
-          <div>
+          <div className="w-full my-6 text-black  ">
             <input
               type="password"
-              className=""
               placeholder="password"
               ref={passwordRef}
               required
+              className="px-1 text-lg"
             />
           </div>
-          <div>
-            <button type="submit">LogIn</button>
+          <div className="w-full my-10">
+            <button
+              type="submit"
+              className="p-2 rounded shadow w-full bg-gradient-to-tr from-gray-400 to-gray-200 text-pink-700 font-bold"
+            >
+              LogIn
+            </button>
           </div>
         </form>
       </div>
