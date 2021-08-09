fb7c2197f68e0cbf7e3dc1452ec4a4a26aa60c6b
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-08-09 15:37:51 +0530

    lil change

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,8 +4,8 @@ import Images from '../components/Images'
 export default function Home() {
   return (
     <>
-      <h1 className="m-auto mb-3 text-3xl text-center mt-7">
-        Welcome To NG Gallery
+      <h1 className="m-auto mb-3 text-3xl text-center mt-7  text-purple-900">
+        Welcome To Web Gallery
       </h1>
       <section className="flex justify-center mx-auto text-center">
         <div className="w-10/12">
